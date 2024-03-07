65f21fb139aebbf51602c4f81ac017b4e79d263c
AlinaSid <alinanice@list.ru>
2024-03-06 21:01:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+
+
 import './App.css';
+import { useTranslation } from 'react-i18next';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const {i18n, t} = useTranslation();
+  
+ const toggleLang = () => {
+  i18n.changeLanguage(i18n.language === 'en'? 'ru':'en')
+ }
+
+  return ( 
+    <div>
+<h1>{t('Welcome to React')}</h1>
+<button onClick={toggleLang}>
+{i18n.language === 'en' ? 'RU' : 'EN'}
+</button>
+
     </div>
-  );
+  )
 }
+
 
 export default App;
